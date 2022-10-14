0e9ddbfe7d8548eac01262161bc10c25dac4e535
thaitrung <trungvnt2000@gmail.com>
2022-10-14 16:40:17 +0700

    add

--- src/js/index.js
+++ src/js/index.js
@@ -15,7 +15,7 @@ $('.multiple-items').slick({
     dots: true,
     infinite: true,
     slidesToShow: 4,
-    slidesToScroll: 2,
+    slidesToScroll: 3,
     autoplay: true,
     autoplaySpeed: 1000
   });
---
+++ src/js/product-detail.js
@@ -0,0 +1,7 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
+import "bootstrap-icons/font/bootstrap-icons.css"
+
+import $ from "jquery"
+
+import "../css/style.css"

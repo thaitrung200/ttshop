eecb045e1a09728f5fca8646c40b2c0944f9738d
thaitrung <trungvnt2000@gmail.com>
2022-10-20 21:01:59 +0700

    add

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
             input: {
                 index: resolve(__dirname, "src/index.html"),
                 about: resolve(__dirname, "src/about.html"),
-                product: resolve(__dirname, "src/product.html"),
+                product: resolve(__dirname, "./src/product.html"),
                 productdetail: resolve(__dirname, "src/product-detail.html"),
                 cart: resolve(__dirname, "src/cart.html"),
             },

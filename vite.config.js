50a45ba2c8dc122fd48562bd4e48fa178b89780b
thaitrung <trungvnt2000@gmail.com>
2022-10-20 21:11:43 +0700

    add

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
     root: "./src",
     build: {
-        outDir: "dist",
         rollupOptions: {
             input: {
                 index: resolve(__dirname, "src/index.html"),

10d96c76e75f68480db6d91ba2d3694d30de603a
thaitrung <trungvnt2000@gmail.com>
2022-10-25 20:28:24 +0700

    add product

--- src/js/product-detail.js
+++ src/js/product-detail.js
@@ -2,10 +2,121 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
 import "bootstrap-icons/font/bootstrap-icons.css"
 
+import _ from "lodash"
 import $ from "jquery"
 
+import "./index.js"
+import "./product.js"
 import "../css/style.css"
 import "../css/reponsive.css"
+
+
+import { productHome } from "./db.js";
+
+$(function () {
+  const url = new URL(location.href);
+  const id = Number(url.searchParams.get("id"));
+  const product = _.find(productHome, { id });
+  console.log(product);
+  $(".non").html(
+    `
+    <div class="product-detail pt-4 pb-4">
+    <div class="container">
+        <h2 class="mb-4">Chi tiết sản phẩm</h2>
+        <div class="row">
+            <div class="col-md-5">
+                <div class="product-detail-img">
+                    <img src="${product.img}" alt="">
+                </div>
+            </div>
+            <div class="col-md-7">
+                <div class="product-detail-warp">
+                    <div class="product-warp-header">
+                        <h1 class="warp-title">${product.name}</h1>
+                        <p class="mt-3">Thương hiệu: Lacoste</p>
+                        <div class="size mt-3">
+                            <p>Kích thước:</p>
+                            <div class="form-size d-flex mt-2">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+                                    <label class="form-check-label" for="exampleRadios1">
+                                      39
+                                    </label>
+                                  </div>
+                                  <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+                                    <label class="form-check-label" for="exampleRadios2">
+                                      40
+                                    </label>
+                                  </div>
+                                  <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="option3" disabled>
+                                    <label class="form-check-label" for="exampleRadios3">
+                                      41
+                                    </label>
+                                  </div>
+                                  <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+                                    <label class="form-check-label" for="exampleRadios2">
+                                      42
+                                    </label>
+                                  </div>
+                                  <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+                                    <label class="form-check-label" for="exampleRadios2">
+                                      43
+                                    </label>
+                                  </div>
+                            </div>
+                        </div>
+                        <p class="price warp-price mt-3">${product.price} đ</p>
+                    </div>
+                    <div class="product-warp-footer d-flex mt-3">
+                      <div class="btn-group" role="group" aria-label="Basic example">
+                        <button type="button" class="btn btn-light btn-down">
+                            -
+                        </button>
+                        <span class="btn quantity">
+                            1
+                        </span>
+                        <button type="button" class="btn btn-light btn-up">
+                            +
+                        </button>
+                      </div>
+                        <button type="submit" class="btn-add">Thêm vào giỏ hàng</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="product-detail-des">
+    <div class="container">
+        <div class="row">
+            <div class="des-heading">Mô tả</div>
+            <div class="product-des-wrap mt-4">
+                <p>️🔥 Giày Chạy Bộ Nam PEAK Running Ultra Light E222147H</p>
+                <p>🎯Chức năng: hấp thụ sốc, chống trượt, chống mài mòn, thoáng khí, thấm mồ hôi, bao bọc, hỗ trợ, cân bằng, chống va đập, nhẹ, tăng, bật</p>
+                <p>
+                    🎯Cỡ giày: 39 40 41 42 43 44 45</p>
+                <p>
+                    🎯Màu: Đen, Trắng, Xám</p>
+                <p>
+                    🎯Địa điểm thích hợp: chạy bộ ngoài trời và trong nhà</p>
+            </div>
+        </div>
+    </div>
+</div>
+    `
+  );
+});
+
+
+
+
+
+
+
 
 let menuMobile=document.querySelector('.menu-mb .menu-mb-icon');
   let headerNav=document.querySelector('.nav-mb');
--- src/js/product.js
+++ src/js/product.js
@@ -3,9 +3,37 @@ import "bootstrap/dist/js/bootstrap.min.js"
 import "bootstrap-icons/font/bootstrap-icons.css"
 
 import $ from "jquery"
+import _ from "lodash"
+import {productHome} from "./db.js"
 
 import "../css/style.css"
 import "../css/reponsive.css"
+
+
+$(function () {
+  const productTemplate = $("#product-home").html();
+  const product = _.template(productTemplate); // compile
+  console.log("index");
+
+  $(".product").append(
+    _.map(productHome, (p) => {
+      const dom = $(product(p));
+      // dom.find(".add-cart").on("click", p, addToCart);
+      return dom;
+    })
+  );
+});
+
+
+
+
+
+
+
+
+
+
+
 
 let menuMobile=document.querySelector('.menu-mb .menu-mb-icon');
   let headerNav=document.querySelector('.nav-mb');

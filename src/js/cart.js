67cd53bd1161abc45817b7eb40cfe7b9b018d1bc
thaitrung <trungvnt2000@gmail.com>
2022-10-27 23:42:47 +0700

    add total

--- src/js/cart.js
+++ src/js/cart.js
@@ -33,8 +33,15 @@ const increment = (event) => {
     product.quantity += 1;
     const item = $(event.target.closest(".item"));
     item.find(".quantity").text(product.quantity);
+   console.log(product.quantity);
+   console.log(event.data.product.price);
+   console.log(product.quantity * event.data.product.price);
+   product.total = product.quantity * event.data.product.price
+   item.find("#price").text(product.total);
+   console.log("tổng",product.total);
 
     localStorage.setItem("carts", JSON.stringify(cart));
+    total()
 };
 
 const decrement = (event) => {
@@ -45,8 +52,12 @@ const decrement = (event) => {
 
     const item = $(event.target.closest(".item"));
     item.find(".quantity").text(product.quantity);
+    product.total = product.quantity * event.data.product.price
+    item.find("#price").text(product.total);
+    console.log("tổng",product.total);
 
     localStorage.setItem("carts", JSON.stringify(cart));
+    total()
 };
 
 $(function () {
@@ -69,8 +80,21 @@ $(function () {
             return dom;
         })
     );
+
+    total()
 });
- 
+
+const total = () => {
+    let sum = 0;
+    for (let i = 0; i < cart.length; i++) {
+        sum += Number(cart[i].total);
+        console.log(cart[i]);
+        console.log(cart[i].total);
+    }
+    $("#subtotal").text(sum.toFixed(0));
+    $("#total").text(sum.toFixed(0));
+  };
+  
 // $(function(){
 //   let cart = JSON.parse(localStorage.getItem("cart")) || [];
 //   if(cart.length === 0){
